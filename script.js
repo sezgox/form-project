e3e7ca0730d5d1c02970ff9d46a02a4f97fba9b5
Sergio Elias <elias.szg2000@gmail.com>
2023-04-06 13:06:08 +0200

    wip

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
  const form = document.getElementById("myForm");
-    form.addEventListener("submit", (event) => {
-        console.log("addEventListener detected")
-        console.log(document.getElementById("name").value)
 
-});
+    form.addEventListener("submit", (event) => {
+     
+        if(document.getElementById("name").value !==""){
+            alert("NO RELLENASTE TODO LOS APARTADOS IMBECIL");
+       }else {
+        console.log("El else funciona");
+       }
+        console.log(document.getElementById("name").value, document.getElementById("address").value, document.getElementById("age").value, document.getElementById("surname").value);
+         
+    });
+
+
+
+
+
+
+
+
+
+
+
